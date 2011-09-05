d9b5c6ff09e80f81e1aba5ff3b3174bf2f0c9924
Shaun Sephton <shaunsephton@gmail.com>
2011-09-05 13:29:44 +0200

    testrunner

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
+from setuptools.command.test import test
+
+def run_tests(self):
+    from setuptest.runtests import runtests
+    return runtests(self)
+test.run_tests = run_tests
 
 setup(
     name='jmbo-calendar',
     version='0.0.2',
     description='Jmbo calendar app.',
-    long_description = open('README.rst', 'r').read(),
+    long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
     author='Praekelt Foundation',
     author_email='dev@praekelt.com',
     license='BSD',
@@ -13,6 +19,10 @@ setup(
     install_requires = [
         'jmbo',
     ],
+    tests_require=[
+        'django-setuptest',
+    ],
+    test_suite="cal.tests",
     include_package_data=True,
     classifiers = [
         "Programming Language :: Python",
---
+++ test_settings.py
@@ -0,0 +1,12 @@
+DATABASE_ENGINE = 'sqlite3'
+
+INSTALLED_APPS = [
+    'django.contrib.contenttypes',
+    'django.contrib.sites',
+    'cal',
+    'category',
+    'jmbo',
+    'photologue',
+    'publisher',
+    'secretballot',
+]
