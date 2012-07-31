99c8efaaf33085336d9de289bfc4a52772d40296
Rizmari Versfeld <rizmari@praekelt.com>
2012-08-01 00:56:30 +0200

    validate event dates

--- cal/admin.py
+++ cal/admin.py
@@ -1,11 +1,31 @@
 from django.contrib import admin
+from django import forms
 
-from jmbo.admin import ModelBaseAdmin
+from jmbo.admin import ModelBaseAdmin, ModelBaseAdminForm
 
 from cal.models import Calendar, Event
 
 
+class EventAdminForm(ModelBaseAdminForm):
+
+    def clean(self, *args, **kwargs):
+        data = super(EventAdminForm, self).clean(*args, **kwargs)
+        # check that the start is earlier than the end
+        if data['start'] >= data['end']:
+            raise forms.ValidationError('''The event's start date needs
+                    to be earlier than its end date''')
+        # check that repeat_until is after the end of the first event rep
+        if data['repeat_until'] is not None and \
+                data['repeat_until'] < data['end'].date():
+            raise forms.ValidationError('''An event cannot have a repeat
+                    cutoff earlier than the actual event''')
+
+        return data
+
+
 class EventAdmin(ModelBaseAdmin):
+    form = EventAdminForm
+
     list_display = ('title', 'start', 'end', 'next',
         'repeat', 'repeat_until', 'venue')
     list_filter = ('repeat',)
