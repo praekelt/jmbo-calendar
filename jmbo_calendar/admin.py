11122b9614d95a78a25cfab203970f2386472a80
Rizmari Versfeld <rizmari@praekelt.com>
2012-09-14 13:02:29 +0200

    fix bug in form validation code

--- jmbo_calendar/admin.py
+++ jmbo_calendar/admin.py
@@ -11,11 +11,11 @@ class EventAdminForm(ModelBaseAdminForm):
     def clean(self, *args, **kwargs):
         data = super(EventAdminForm, self).clean(*args, **kwargs)
         # check that the start is earlier than the end
-        if data['start'] >= data['end']:
+        if data['start'] and data['end'] and data['start'] >= data['end']:
             raise forms.ValidationError('''The event's start date needs
                     to be earlier than its end date''')
         # check that repeat_until is after the end of the first event rep
-        if data['repeat_until'] is not None and \
+        if data['repeat_until'] and data['end'] and \
                 data['repeat_until'] < data['end'].date():
             raise forms.ValidationError('''An event cannot have a repeat
                     cutoff earlier than the actual event''')
