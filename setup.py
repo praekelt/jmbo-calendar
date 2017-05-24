cfa020b1538796a698dfb6a2c84ce9337915c101
cblignaut <cilliers@praekeltconsulting.com>
2017-05-24 14:40:07 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,23 +2,24 @@ from setuptools import setup, find_packages
 
 setup(
     name="jmbo-calendar",
-    version="2.0.2",
+    version="3.0.0",
     description="Jmbo calendar app.",
-    long_description = open("README.rst", "r").read() + open("AUTHORS.rst", "r").read() + open("CHANGELOG.rst", "r").read(),
-    author="Praekelt Foundation",
+    long_description=(open("README.rst", "r").read() +
+                        open("AUTHORS.rst", "r").read() +
+                        open("CHANGELOG.rst", "r").read()),
+    author="Praekelt Consulting",
     author_email="dev@praekelt.com",
     license="BSD",
     url="http://github.com/praekelt/jmbo-calendar",
-    packages = find_packages(),
-    install_requires = [
-        # "jmbo>=3.0.0", uncomment when released
+    packages=find_packages(),
+    install_requires=[
+        "jmbo>=3.0.0",
         "django-simplemde",
     ],
     tests_require=[
         "tox",
         "psycopg2",
     ],
-    test_suite="setuptest.setuptest.SetupTestSuite",
     include_package_data=True,
     classifiers = [
         "Programming Language :: Python",
